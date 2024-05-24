a2cce3fb6faae1660caafef8cffea3bbfddfe9ae
Luis Alberto Donado <luisinodnd@gmail.com>
2024-05-24 15:45:58 -0500

    Modelo Factura

---
+++ app/Models/Factura.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Factura extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'cita_id',
+        'total',
+        'fecha_emision',
+        'estado'
+    ];
+
+    public function cita()
+    {
+        return $this->belongsTo(Cita::class);
+    }
+}
